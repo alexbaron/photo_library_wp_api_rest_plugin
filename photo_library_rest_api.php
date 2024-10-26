8a639870f40ba782d4c7047a4153cc0ca71026ee
Alex Baron <526473+gb96@users.noreply.github.com>
2024-10-26 15:46:22 -0400

    20241026: First commit

---
+++ photo_library_rest_api.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Photo_library_rest_api
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     photo_library_rest_api
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Photo_library_rest_api
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Photo_library_rest_api
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
